4630c4f481daa6fbd6a3cfee101565b486b925f3
David Dudson <d.dudson@psenterprise.com>
2017-01-24 14:10:15 +1300

    Seperate GitterBot from IRCBot

--- src/main/scala/org/multibot/Multibottest.scala
+++ src/main/scala/org/multibot/Multibottest.scala
@@ -2,23 +2,28 @@ package org.multibot
 
 object Multibottest {
   val GitterPassEnvName = "multibot.gitter.pass"
-  def main(args: Array[String]): Unit = {
-    val cache = InterpretersCache(List("#scala", "#scalaz", "#dev-ua/scala"))
-    val PRODUCTION = Option(System getenv "multibot.production") exists (_.toBoolean)
-    val gitterPass = Option(System getenv GitterPassEnvName).getOrElse("709182327498f5ee393dbb0bc6e440975fa316e5")
-    Multibot(cache, if (PRODUCTION) "multibot_" else "multibot__",
-      if (PRODUCTION)
-        List("#clojure.pl", "#scala.pl", "#scala", "#scalaz", "#scala-fr", "#lift", "#playframework",
-          "#bostonpython", "#fp-in-scala", "#CourseraProgfun", "#shapeless", "#akka", "#sbt", "#scala-monocle")
-      else
-        List("#multibottest", "#multibottest2")
-    ).start()
-    Multibot(cache = cache,
-      botname = if (PRODUCTION) "multibot1" else "multibot2",
-      channels = if (PRODUCTION) List("#scala/scala", "#sbt/sbt") else List("#OlegYch/multibot"),
-      settings = _.setServerHostname("irc.gitter.im").setServerPassword(gitterPass).
+  val cache = InterpretersCache(List("#scala", "#scalaz", "#dev-ua/scala"))
+  val PRODUCTION = Option(System getenv "multibot.production") exists (_.toBoolean)
+  val gitterPass = Option(System getenv GitterPassEnvName).getOrElse("709182327498f5ee393dbb0bc6e440975fa316e5")
+
+  val ircMultibot = Multibot(cache, if (PRODUCTION) "multibot_" else "multibot__",
+    if (PRODUCTION)
+      List("#clojure.pl", "#scala.pl", "#scala", "#scalaz", "#scala-fr", "#lift", "#playframework",
+        "#bostonpython", "#fp-in-scala", "#CourseraProgfun", "#shapeless", "#akka", "#sbt", "#scala-monocle")
+    else
+      List("#multibottest", "#multibottest2")
+  )
+
+  val gitterMultibot = Multibot(cache = cache,
+    botname = if (PRODUCTION) "multibot1" else "multibot2",
+    channels = if (PRODUCTION) List("#scala/scala", "#sbt/sbt") else List("#OlegYch/multibot"),
+    settings = _.setServerHostname("irc.gitter.im").setServerPassword(gitterPass).
         setSocketFactory(javax.net.ssl.SSLSocketFactory.getDefault)
-    ).start()
+  )
+
+  def main(args: Array[String]): Unit = {
+    ircMultibot.start()
+    gitterMultibot.start()
     while (scala.io.StdIn.readLine() != "exit") Thread.sleep(1000)
     sys.exit()
   }
